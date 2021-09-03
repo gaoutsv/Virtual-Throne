b13fa3250298776593e7ade18b0c10615ef39157
gaoutsv <ggomes@unigranrio.br>
2021-09-03 14:35:06 -0300

    Full Project
    Commit com todo projeto

---
+++ README.md
@@ -0,0 +1,7 @@
+# A Site of Ice & Fire
+Projeto Curricular Articulador do 1º Periodo dos Cursos de Sistemas da Informação e Analise e Desenvolvimento de Sistemas
+# Integrantes: 
+<br>Leonardo dos Santos Boa Morte.||Link do Github: https://github.com/Boa-Morte
+<br>Werner Romling Salustiano.    ||Link do Github: https://github.com/Werner1201
+<br>Gustavo Gomes.                ||Link do Github: https://github.com/gaoutsv
+<!--TODO: Falta Acrescentar alguns links e integrantes.--!>
---
+++ _JavaScript/funcoes.js
@@ -0,0 +1,84 @@
+/*** Created by Werner Römling on 01/04/2017.***/
+function mudaFrame (frame)
+{
+    document.getElementById("frame").src = frame ;
+}
+
+//função para mostrar os elementos de imagem no plano flutuante. - Feito por Boa Morte
+function mostra(){
+document.getElementById('planof').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante. - Feito por Boa Morte
+function esconde(){
+document.getElementById('planof').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra02(){
+document.getElementById('planof02').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde02(){
+document.getElementById('planof02').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra03(){
+document.getElementById('planof03').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde03(){
+document.getElementById('planof03').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra04(){
+document.getElementById('planof04').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde04(){
+document.getElementById('planof04').style.visibility="hidden";
+}
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra05(){
+document.getElementById('planof05').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde05(){
+document.getElementById('planof05').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra06(){
+document.getElementById('planof06').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde06(){
+document.getElementById('planof06').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra07(){
+document.getElementById('planof07').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde07(){
+document.getElementById('planof07').style.visibility="hidden";
+}
+
+//função para mostrar os elementos de imagem no plano flutuante (wallpaper).
+function mostra08(){
+document.getElementById('planof08').style.visibility="visible";
+}
+
+//função para esconder os elementos de imagem no plano flutuante (wallpaper).
+function esconde08(){
+document.getElementById('planof08').style.visibility="hidden";
+}
